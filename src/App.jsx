45551130f65f4e227d3581c0bb6f6286782fe1a3
vernoxy <vernoxy3@gmail.com>
2026-02-13 16:35:26 +0530

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home/Home';
-import AboutUs from './pages/AboutsUs/AboutUs';
-import Products from './pages/Products/Products';
-import ContactUs from './pages/ContactUs/ContactUs';
-import Footer from './components/Footer/Footer';
-import ScrollToTop from './ScrollToTop';
-import Aos from 'aos';
-import 'aos/dist/aos.css'; // Make sure to import AOS CSS
+import React, { useEffect, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./ScrollToTop";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Logo from "./assets/RSLogo.svg";
+
+
+// Lazy Load Pages
+const Home = lazy(() => import("./pages/Home/Home"));
+const AboutUs = lazy(() => import("./pages/AboutsUs/AboutUs"));
+const Products = lazy(() => import("./pages/Products/Products"));
+const ContactUs = lazy(() => import("./pages/ContactUs/ContactUs"));
 
 const App = () => {
   useEffect(() => {
     Aos.init({
       duration: 1200,
-      once: true, // Ensures animation only runs once
-      offset: 200, // Controls when the animation triggers
+      once: true,
+      offset: 200,
     });
   }, []);
 
@@ -25,13 +29,27 @@ const App = () => {
     <Router>
       <ScrollToTop />
       <Navbar />
+
       <div className="overflow-x-hidden">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<ContactUs />} />
-        </Routes>
+        {/* Suspense Wrapper */}
+        <Suspense
+          fallback={
+            <div className="flex flex-col justify-center items-center h-screen text-xl space-y-5">
+
+              <img src={Logo} alt="" loading="lazy" className="animate-pulse"/>
+              <p>Loading...</p>
+
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/contact" element={<ContactUs />} />
+          </Routes>
+        </Suspense>
+
         <Footer />
       </div>
     </Router>
